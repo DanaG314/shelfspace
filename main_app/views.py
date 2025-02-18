da8087a0784ae041e65124be1a496a3f21010eab
Patrick Simmons <hadroncollides@gmail.com>
2025-02-18 13:43:06 -0800

    search debugging

--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.conf import settings
 
 class Home(LoginView):
     template_name = 'home.html'
@@ -41,26 +42,28 @@ def book_detail(request, book_id):
 def book_search(request):
     search_results = []
     if 'q' in request.GET:
-        query = request.Get('q', '')
+        query = request.GET.get('q', '')
         api_key = settings.GOOGLE_BOOKS_API_KEY
         url = f'https://www.googleapis.com/books/v1/volumes?q={query}&key={api_key}'
 
         try:
-                response = requests.get(url)
-                if response.status_code == 200:
-                    data = response.json()
-                    if 'items' in data:
-                        for item in data['items']:
-                            volume_info = item.get('volumeInfo', {})
-                            book_data = {
-                                'id': item.get('id'),
-                                'title': volume_info.get('title', 'Unknown Title'),
-                                'authors': volume_info.get('authors', ['Unknown Author'])[0],
-                                'cover_url': volume_info.get('imageLinks', {}).get('thumbnail', ''),
-                                'isbn_13': next((id_info['identifier'] for id_info in volume_info.get('industryIdentifiers', [])
-                                          if id_info['type'] == 'ISBN_13'), None),
-                                'page_count': volume_info.get('pageCount', 0),
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                if 'items' in data:
+                    for item in data['items']:
+                        volume_info = item.get('volumeInfo', {})
+                        book_data = {
+                            'id': item.get('id'),
+                            'title': volume_info.get('title', 'Unknown Title'),
+                            'authors': volume_info.get('authors', ['Unknown Author'])[0],
+                            'cover_url': volume_info.get('imageLinks', {}).get('thumbnail', ''),
+                            'isbn_13': next((id_info['identifier'] for id_info in volume_info.get('industryIdentifiers', [])
+                                      if id_info['type'] == 'ISBN_13'), None),
+                            'page_count': volume_info.get('pageCount', 0),
                         }
-                    search_results.append(book_data)
+                        search_results.append(book_data)
         except requests.RequestException as e:
             print(f"Error fetching books: {e}")
+    
+    return render(request, 'books/search.html', {'results': search_results})
