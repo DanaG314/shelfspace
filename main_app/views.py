bd2e829df1aa9e7fc6ebd452ccfe1afd54203057
Patrick Simmons <hadroncollides@gmail.com>
2025-02-18 09:03:06 -0800

    logout

--- main_app/views.py
+++ main_app/views.py
@@ -1,31 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-class Home(LoginView):
-    template_name = "home.html"
-
-def about(request):
-    return render(request, 'about.html')
 
 class Home(LoginView):
     template_name = 'home.html'
 
-def signup(request):
-  error_message = '&nbsp;'
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('cat-index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'signup.html', context)
-
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
