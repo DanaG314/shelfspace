5595189d48bf42b62b9e1b54bc352716989e9935
KoreyOcampo <koreyocampo@gmail.com>
2025-02-19 12:54:05 -0800

    create

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('bookshelf/<int:book_id>/', views.book_detail, name='bookshelf-detail'),
     path('bookshelf/search/', views.book_search, name='book-search'),
     path('bookshelf/add/', views.book_add, name='book-add'),
-    path('bookshelf/create/', views.BookCreate.as_view(), name='book-create'),
+    path('books/<int:book_id>/notes/', views.update_notes, name='update-notes'),
+    path('books/<int:book_id>/progress/', views.update_progress, name='update-progress'),
     #path('books/<int:book_id>/update/', views.book_update, name='book-update'),
     #path('books/<int:book_id>/delete/', views.book_delete, name='book-delete'),
 ]
