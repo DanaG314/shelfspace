9f28dd46685c4578fa2fa278a821e1f9b46e620c
Patrick Simmons <hadroncollides@gmail.com>
2025-02-19 07:43:20 -0800

    api working

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('bookshelf/', views.bookshelf, name='bookshelf'),
     path('bookshelf/<int:book_id>/', views.book_detail, name='bookshelf-detail'),
     path('bookshelf/search/', views.book_search, name='book-search'),
+    path('bookshelf/add/', views.book_add, name='book-add'),
 ]
